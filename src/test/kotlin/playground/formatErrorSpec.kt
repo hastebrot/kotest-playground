174191602cf61749c6d6700d958df65d818a337e
Benjamin Gudehus <benjamin.gudehus@freiheit.com>
2022-03-24 22:16:57 +0100

    (test) Rename to FormatErrorSpec.

---
+++ src/test/kotlin/playground/formatErrorSpec.kt
@@ -0,0 +1,29 @@
+package playground
+
+import io.kotest.assertions.Actual
+import io.kotest.assertions.Expected
+import io.kotest.assertions.eq.eq
+import io.kotest.assertions.failure
+import io.kotest.assertions.print.print
+import io.kotest.core.spec.style.ExpectSpec
+import io.kotest.matchers.shouldBe
+
+class FormatErrorSpec : ExpectSpec({
+    context("intellij format errors") {
+        expect("with shouldBe") {
+            "foobar".shouldBe("foo")
+        }
+
+        expect("with eq") {
+            eq("foobar", "foo")?.let { throw it }
+        }
+
+        expect("with failure") {
+            failure(Expected("foo".print()), Actual("foobar".print())).let { throw it }
+        }
+
+        expect("with Error") {
+            throw Error("""expected:<"foo"> but was:<"foobar">""")
+        }
+    }
+})
